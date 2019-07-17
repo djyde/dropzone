0ee1744e23fb3c6cf944aaf0074c3bdeaec93c81
Randy <randypriv@gmail.com>
2019-07-17 23:03:50 +0800

    Initial Commit

---
+++ dropzone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dropzone
+//
+//  Created by Randy Lu on 2019/7/17.
+//  Copyright © 2019 Randy Lu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dropzone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dropzone
+//
+//  Created by Randy Lu on 2019/7/17.
+//  Copyright © 2019 Randy Lu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
